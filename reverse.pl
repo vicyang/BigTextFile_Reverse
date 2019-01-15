e09cf3284665064b94f10b3d061008d939ca5f17
Vulkan <523066680@163.com>
2019-01-15 17:48:30 +0800

    改为仅存储索引，不存储长度，内存占用620MB

--- reverse.pl
+++ reverse.pl
@@ -1,16 +1,16 @@
 =info
-    second
-    理论上这种操作是对的，
-    问题是，当行数超过一定量，数组列表也会爆
+    1. 当Perl数组存储的索引超过一定量，也会提示 out_of_memory
+    2. 不以哈希结构存储 pos 和 length 信息。 3GB文件，1024*1024*20行
+       耗时 15.2秒，占用600MB。第二次执行因为有缓存，耗时7.7秒
+       将 unshift 改为push，耗时差异不大
 =cut
 
+use Devel::Size qw(size total_size);
 use File::Basename;
 STDOUT->autoflush(1);
-my $src="F:/A_Parts.txt";
+my $src="F:/A.txt";
 my $dst=$src;
 $dst=~s/(\.\w+)$/_REV$1/;
-print $dst;
-exit;
 
 open my $fh, "<:raw", $src or die "$!\n";
 my $s;
@@ -22,15 +22,17 @@ while ( !eof($fh) )
 {
     $s = readline($fh);
     $pos = tell($fh);
-    unshift @index, { 'pos' => $prev, 'len' => $pos-$prev };
+    unshift @index, $prev;
     $prev = $pos;
 }
 printf "Done\n";
+printf "%d %d\n", $#index, $index[0];
+printf "%d\n", total_size(\@index)/(1024*1024);
 
-for my $idx (@index) 
-{
-    seek($fh, $idx->{pos}, SEEK_SET);
-    read( $fh, $s, $idx->{len} );
-    $s=~s/\r\n//;
-    printf "%s\n", $s; 
-}
+# for my $idx (@index) 
+# {
+#     seek($fh, $idx->{pos}, SEEK_SET);
+#     read( $fh, $s, $idx->{len} );
+#     $s=~s/\r\n//;
+#     printf "%s\n", $s; 
+# }
