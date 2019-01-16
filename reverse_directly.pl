a9806618fcd86dec39cc097778cad416e2c8cdab
Vulkan <523066680@163.com>
2019-01-16 09:40:30 +0800

    直接逐个字节处理，效率极低

--- reverse_directly.pl
+++ reverse_directly.pl
@@ -5,7 +5,7 @@
 =cut
 
 use strict;
-use Devel::Size qw(size total_size);
+use Fcntl qw(:seek);
 use File::Basename;
 STDOUT->autoflush(1);
 my $src = "F:/A_Parts.txt";
@@ -18,7 +18,21 @@ sub reverse_write
 {
     my ($srcfile, $dstfile) = @_;
     open my $SRC, "<:raw", $srcfile or die "$!\n";
-    
+    open my $DST, ">:raw", $dstfile or die "$!\n";
+    my $offset = (-s $srcfile) - 1;
+    my $char;
+    my $buff;
+    while ($offset >= 1) 
+    {
+        seek $SRC, $offset, SEEK_SET;
+        read $SRC, $char, 1;
+        $buff .= $char;
+        if ( $char eq "\n" ) { 
+            $buff = "";
+            #printf "%d\n", tell $SRC;
+        }
+        $offset--;
+    }
 
 }
 
