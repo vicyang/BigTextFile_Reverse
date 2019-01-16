85271eeba15c122660ff436c17890ae04ab3396f
Vulkan <523066680@163.com>
2019-01-16 15:53:25 +0800

    精简代码。已知问题：split 默认操作下会清除数组末尾的空白数据，例如split(",", "a,b,c,,,")， 末尾连续逗号之间的“空白”丢失

--- reverse_directly.pl
+++ reverse_directly.pl
@@ -7,9 +7,9 @@
 use strict;
 use Fcntl qw(:seek);
 STDOUT->autoflush(1);
-my $src = "F:/A.txt";
+my $src = "F:/A_Parts.txt";
 my $dst = $src;
-   $dst =~s/(\.\w+)$/_REV$1/;
+$dst =~s/(\.\w+)$/_REV$1/;
 
 reverse_write( $src, $dst );
 
@@ -18,22 +18,19 @@ sub reverse_write
     my ($srcfile, $dstfile) = @_;
     open my $SRC, "<:raw", $srcfile or die "$!\n";
     open my $DST, ">:raw", $dstfile or die "$!\n";
-    my $total = -s $SRC;
+
+    # 缓冲区大小
     my $buffsize = 2**16;
-    my $offset = $total;
+    my $offset = -s $SRC;
     my $buff;
     my @lines;
     my $left = "";
-    my $count = 0;
-    seek $SRC, 0, SEEK_END;
     while ( $offset >= $buffsize )
     {
-        $count++;
         $offset -= $buffsize;
         seek $SRC, $offset, SEEK_SET;
         read $SRC, $buff, $buffsize;
         $buff = $buff . $left;
-
         if ( $buff =~/\r?\n/ ) {
             @lines = reverse( split /\r?\n/, $buff );
             $left = pop @lines;
@@ -44,10 +41,9 @@ sub reverse_write
         }
     }
 
-    #printf "%d %d %d %d\n", $count, $count*$buffsize, (-s $srcfile) - $count*$buffsize, $offset;
+    return if ($offset <= 0);
     seek $SRC, 0, SEEK_SET;
     read $SRC, $buff, $offset;
     @lines = reverse(split /\r?\n/, $buff .$left );
     print $DST join("\r\n", @lines);
-
 }
