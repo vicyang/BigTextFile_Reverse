096e42322c15e83cdcbc6a387a25218c4897062e
Vulkan <523066680@163.com>
2019-01-16 15:28:02 +0800

    初步无误，3G文件耗时 110秒

--- reverse_directly.pl
+++ reverse_directly.pl
@@ -16,35 +16,41 @@ reverse_write( $src, $dst );
 sub reverse_write
 {
     my ($srcfile, $dstfile) = @_;
-    open my $SRC, "<:raw:crlf", $srcfile or die "$!\n";
+    open my $SRC, "<:raw", $srcfile or die "$!\n";
     open my $DST, ">:raw", $dstfile or die "$!\n";
-    my $buffsize = 2 ** 8;
-    my $offset = -s $srcfile;
-    my $pool;
+    my $total = -s $SRC;
+    my $buffsize = 2**16;
+    my $offset = 0;
     my $buff;
     my @lines;
-    my $after;
-    my $before;
-    while ($offset >= $buffsize)
+    my $left = "";
+    my $count = 0;
+    seek $SRC, 0, SEEK_END;
+    while ( $offset < $total-$buffsize )
     {
-        $offset-=$buffsize;
-        seek $SRC, $offset, SEEK_SET;
+        $count++;
+        $offset += $buffsize;
+        seek $SRC, -$buffsize, SEEK_CUR;
         read $SRC, $buff, $buffsize;
-        #if ( $buff=~/\n$/ ) { $after = "\n" } else { $after = "" }
-        # if ( $buff=~/^\r?\n/ ) { $before = "\n" } else { $before = "" }
-        @lines = reverse(split "\n", $buff);
-        printf "%s%s%s:", $before,  join("\n", @lines), $after;
-        #unless ($lines[-1]=~/\n$/) { print "\n" }
-        $buff = "";
-        @lines = ();
-        exit;
+        seek $SRC, -$buffsize, SEEK_CUR;
+        $buff = $buff . $left;
+
+        if ( $buff =~/\r?\n/ ) {
+            @lines = reverse( split /\r?\n/, $buff );
+            $left = pop @lines;
+            printf $DST "%s\r\n", join("\r\n", @lines);
+        } else {
+            $left = $buff;
+            #printf "%s\n", $left;
+        }
     }
 
     #printf "%d %d %d %d\n", $count, $count*$buffsize, (-s $srcfile) - $count*$buffsize, $offset;
 
+    $offset = tell $SRC;
     seek $SRC, 0, SEEK_SET;
     read $SRC, $buff, $offset;
-    @lines = reverse(split /\r?\n/, $buff);
-    print join("\n", @lines);
+    @lines = reverse(split /\r?\n/, $buff .$left );
+    print $DST join("\r\n", @lines);
 
 }
